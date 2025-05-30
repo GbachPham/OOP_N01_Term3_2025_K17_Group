1d8fc573d74f59d5a11b41246ae6798c5d04faee
Trugan20 <23010339@st.phenikaa-uni.edu.vn>
2025-05-30 06:23:49 +0000

    Nem chua

--- test/src/Hoadon.java
+++ test/src/Hoadon.java
@@ -4,24 +4,77 @@ public class Hoadon {
     private double tongTien;
 
     public Hoadon(String maHoaDon, String ngayBan, double tongTien) {
-        this.maHoaDon = maHoaDon;
-        this.ngayBan = ngayBan;
-        this.tongTien = tongTien;
+        try {
+            this.maHoaDon = maHoaDon;
+            this.ngayBan = ngayBan;
+            this.tongTien = tongTien;
+        } catch (Exception e) {
+            System.out.println("Lỗi khởi tạo Hoadon: " + e.getMessage());
+        }
     }
 
-    public String getMaHoaDon() { return maHoaDon; }
-    public void setMaHoaDon(String maHoaDon) { this.maHoaDon = maHoaDon; }
-    public String getNgayBan() { return ngayBan; }
-    public void setNgayBan(String ngayBan) { this.ngayBan = ngayBan; }
-    public double getTongTien() { return tongTien; }
-    public void setTongTien(double tongTien) { this.tongTien = tongTien; }
+    public String getMaHoaDon() {
+        try {
+            return maHoaDon;
+        } catch (Exception e) {
+            System.out.println("Lỗi getMaHoaDon: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void setMaHoaDon(String maHoaDon) {
+        try {
+            this.maHoaDon = maHoaDon;
+        } catch (Exception e) {
+            System.out.println("Lỗi setMaHoaDon: " + e.getMessage());
+        }
+    }
+
+    public String getNgayBan() {
+        try {
+            return ngayBan;
+        } catch (Exception e) {
+            System.out.println("Lỗi getNgayBan: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public void setNgayBan(String ngayBan) {
+        try {
+            this.ngayBan = ngayBan;
+        } catch (Exception e) {
+            System.out.println("Lỗi setNgayBan: " + e.getMessage());
+        }
+    }
+
+    public double getTongTien() {
+        try {
+            return tongTien;
+        } catch (Exception e) {
+            System.out.println("Lỗi getTongTien: " + e.getMessage());
+            return 0;
+        }
+    }
+
+    public void setTongTien(double tongTien) {
+        try {
+            this.tongTien = tongTien;
+        } catch (Exception e) {
+            System.out.println("Lỗi setTongTien: " + e.getMessage());
+        }
+    }
 
     @Override
     public String toString() {
-        return "Hoadon{" +
-                "maHoaDon='" + maHoaDon + '\'' +
-                ", ngayBan='" + ngayBan + '\'' +
-                ", tongTien=" + tongTien +
-                '}';
+        try {
+            return "Hoadon{" +
+                    "maHoaDon='" + maHoaDon + '\'' +
+                    ", ngayBan='" + ngayBan + '\'' +
+                    ", tongTien=" + tongTien +
+                    '}';
+        } catch (Exception e) {
+            System.out.println("Lỗi toString: " + e.getMessage());
+            return "";
+        }
     }
 }
--- test/src/HoadonManager.java
+++ test/src/HoadonManager.java
@@ -6,40 +6,61 @@ public class HoadonManager {
 
     // Create
     public void themHoaDon(Hoadon hoadon) {
-        danhSachHoaDon.add(hoadon);
+        try {
+            danhSachHoaDon.add(hoadon);
+        } catch (Exception e) {
+            System.out.println("Lỗi thêm hóa đơn: " + e.getMessage());
+        }
     }
 
     // Read
     public List<Hoadon> layTatCaHoaDon() {
-        return danhSachHoaDon;
+        try {
+            return danhSachHoaDon;
+        } catch (Exception e) {
+            System.out.println("Lỗi lấy danh sách hóa đơn: " + e.getMessage());
+            return new ArrayList<>();
+        }
     }
 
     public Hoadon timHoaDonTheoMa(String maHoaDon) {
-        for (Hoadon hd : danhSachHoaDon) {
-            if (hd.getMaHoaDon().equals(maHoaDon)) {
-                return hd;
+        try {
+            for (Hoadon hd : danhSachHoaDon) {
+                if (hd.getMaHoaDon().equals(maHoaDon)) {
+                    return hd;
+                }
             }
+        } catch (Exception e) {
+            System.out.println("Lỗi tìm hóa đơn theo mã: " + e.getMessage());
         }
         return null;
     }
 
     // Update
     public boolean capNhatHoaDon(String maHoaDon, String ngayBanMoi, double tongTienMoi) {
-        Hoadon hd = timHoaDonTheoMa(maHoaDon);
-        if (hd != null) {
-            hd.setNgayBan(ngayBanMoi);
-            hd.setTongTien(tongTienMoi);
-            return true;
+        try {
+            Hoadon hd = timHoaDonTheoMa(maHoaDon);
+            if (hd != null) {
+                hd.setNgayBan(ngayBanMoi);
+                hd.setTongTien(tongTienMoi);
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("Lỗi cập nhật hóa đơn: " + e.getMessage());
         }
         return false;
     }
 
     // Delete
     public boolean xoaHoaDon(String maHoaDon) {
-        Hoadon hd = timHoaDonTheoMa(maHoaDon);
-        if (hd != null) {
-            danhSachHoaDon.remove(hd);
-            return true;
+        try {
+            Hoadon hd = timHoaDonTheoMa(maHoaDon);
+            if (hd != null) {
+                danhSachHoaDon.remove(hd);
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("Lỗi xóa hóa đơn: " + e.getMessage());
         }
         return false;
     }
